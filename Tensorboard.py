ec45c70a7e370102c515bae2c7b3f2b51b4006b6
tummalapallimurali <tummalapallimurali@gmail.com>
2022-12-15 16:54:02 -0500

    Add files via upload

---
+++ Tensorboard.py
@@ -0,0 +1,82 @@
+import gym
+import argparse
+import mlflow
+import sinergym
+
+from sinergym.utils.callbacks import LoggerCallback, LoggerEvalCallback
+from sinergym.utils.wrappers import NormalizeObservation
+
+
+from stable_baselines3.common.noise import NormalActionNoise, OrnsteinUhlenbeckActionNoise
+from stable_baselines3 import A2C, DDPG, DQN, PPO, SAC
+from stable_baselines3.common.callbacks import EvalCallback, BaseCallback, CallbackList
+from stable_baselines3.common.vec_env import DummyVecEnv
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--environment', '-env', type=str, default=None)
+parser.add_argument('--episodes', '-ep', type=int, default=1)
+parser.add_argument('--learning_rate', '-lr', type=float, default=.0007)
+parser.add_argument('--n_steps', '-n', type=int, default=5)
+parser.add_argument('--gamma', '-g', type=float, default=.99)
+parser.add_argument('--gae_lambda', '-gl', type=float, default=1.0)
+parser.add_argument('--ent_coef', '-ec', type=float, default=0)
+parser.add_argument('--vf_coef', '-v', type=float, default=.5)
+parser.add_argument('--max_grad_norm', '-m', type=float, default=.5)
+parser.add_argument('--rms_prop_eps', '-rms', type=float, default=1e-05)
+args = parser.parse_args()
+
+# experiment ID
+environment = args.environment
+n_episodes = args.episodes
+name = 'A2C' ##+ environment + '-' + str(n_episodes) + '-episodes'
+
+with mlflow.start_run(run_name=name):
+
+    mlflow.log_param('env', environment)
+    mlflow.log_param('episodes', n_episodes)
+
+    mlflow.log_param('learning_rate', args.learning_rate)
+    mlflow.log_param('n_steps', args.n_steps)
+    mlflow.log_param('gamma', args.gamma)
+    mlflow.log_param('gae_lambda', args.gae_lambda)
+    mlflow.log_param('ent_coef', args.ent_coef)
+    mlflow.log_param('vf_coef', args.vf_coef)
+    mlflow.log_param('max_grad_norm', args.max_grad_norm)
+    mlflow.log_param('rms_prop_eps', args.rms_prop_eps)
+
+    env = gym.make(environment)
+    env = NormalizeObservation(env)
+
+    #### TRAINING ####
+
+    # Build model
+
+    model = A2C('MlpPolicy', env, verbose=1,
+                 learning_rate=args.learning_rate,
+                 n_steps=args.n_steps,
+                 gamma=args.gamma,
+                 gae_lambda=args.gae_lambda,
+                 ent_coef=args.ent_coef,
+                 vf_coef=args.vf_coef,
+                 max_grad_norm=args.max_grad_norm,
+                 rms_prop_eps=args.rms_prop_eps,
+                 tensorboard_log='./tensorboard_log/')
+
+
+    n_timesteps_episode = env.simulator._eplus_one_epi_len / \
+        env.simulator._eplus_run_stepsize
+    timesteps = n_episodes * n_timesteps_episode
+
+    env = DummyVecEnv([lambda: env])
+
+    # Callbacks
+    freq = 2  # evaluate every N episodes
+    eval_callback = LoggerEvalCallback(env, best_model_save_path='./best_models/' + name + '/',
+                                    log_path='./best_models/' + name + '/', eval_freq=n_timesteps_episode * freq,
+                                    deterministic=True, render=False, n_eval_episodes=1)
+    log_callback = LoggerCallback(sinergym_logger=False)
+    callback = CallbackList([log_callback, eval_callback])
+
+    # Training
+    model.learn(total_timesteps=timesteps, callback=callback, log_interval=100)
